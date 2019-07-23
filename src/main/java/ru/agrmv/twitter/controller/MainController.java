a02cbdcb7ab5f326b70a792840d73ecbb7568dd5
Aleksey <gromov-aleksey@outlook.com>
2019-07-23 20:01:44 +0700

    Добавил возможность регистрации пользователей

--- src/main/java/ru/agrmv/twitter/controller/MainController.java
+++ src/main/java/ru/agrmv/twitter/controller/MainController.java
@@ -10,40 +10,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.agrmv.twitter.model.Message;
 import ru.agrmv.twitter.repository.MessageRepository;
 
-import java.util.List;
-
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String startPage() {
+        return "startPage";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Model model) {
         model.addAttribute("messages", messageRepository.findAll());
-        return "main";
+        return "mainPage";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Model model) {
         messageRepository.save(new Message(text, tag));
         model.addAttribute("messages", messageRepository.findAll());
-        return "main";
+        return "mainPage";
     }
 
-    @PostMapping("/filter")
+    @PostMapping("filter")
     public String filter(@RequestParam String tag, Model model) {
         if (tag != null && !tag.isEmpty()) {
             model.addAttribute("messages", messageRepository.findByTag(tag));
         } else {
             model.addAttribute("messages", messageRepository.findAll());
         }
-        return "main";
+        return "mainPage";
     }
 }
