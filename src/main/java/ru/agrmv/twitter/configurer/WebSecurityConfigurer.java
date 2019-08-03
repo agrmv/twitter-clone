bc74545d6acad7dbdf445cdbba64a7099042c1a5
Aleksey <gromov-aleksey@outlook.com>
2019-08-04 00:18:54 +0700

    Доделал загрузку файлов

--- src/main/java/ru/agrmv/twitter/configurer/WebSecurityConfigurer.java
+++ src/main/java/ru/agrmv/twitter/configurer/WebSecurityConfigurer.java
@@ -1,17 +1,14 @@
 package ru.agrmv.twitter.configurer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import ru.agrmv.twitter.service.UserService;
-
 
 
 @Configuration
@@ -48,10 +45,5 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
         /*Менеджер учетных записей*/
         auth.userDetailsService(userService)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }
-
-    @Bean
-    GrantedAuthorityDefaults grantedAuthorityDefaults() {
-        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
     }
 }
